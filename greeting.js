2b14338d2ea186309e89435f1224745e7d4db093
ChicRock <jungsungrak@gmail.com>
2019-04-18 19:08:30 +0900

    Saving the username in the localstorage

---
+++ greeting.js
@@ -0,0 +1,43 @@
+const form = document.querySelector(".js-form"),
+  input = form.querySelector("input"),
+  greeting = document.querySelector(".js-greetings");
+
+const USER_LS = "currentUser",
+  SHOWING_CN = "showing";
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  paintGreeting(currentValue);
+  saveName(currentValue);
+}
+
+function askForName() {
+  form.addEventListener("submit", handleSubmit);
+  form.classList.add(SHOWING_CN);
+}
+
+function paintGreeting(text) {
+  greeting.classList.add(SHOWING_CN);
+  form.classList.remove(SHOWING_CN);
+
+  greeting.innerText = `Hello ${text}`;
+}
+
+function loadName() {
+  const currentUser = localStorage.getItem(USER_LS);
+
+  if (currentUser === null) {
+    askForName();
+  } else {
+    paintGreeting(currentUser);
+  }
+}
+
+(function() {
+  loadName();
+})();
